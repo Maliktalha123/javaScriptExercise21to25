c6afb420dc21ee7aa5267b5557ef5a2e77612bb3
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-04-17 23:56:43 +0500

    This is Excercises of Java Script

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+// 1
+let firstName = prompt("Enter your first name");
+let lastName = prompt("Enter your last name");
+alert(`Hello Mr.${firstName} ${lastName} .Welcome to our Store`)
+// 2
+let favouriteMobile = prompt("Enter Your Favourite Mobile Phone Model");
+let lengthOfMobilePhone = favouriteMobile.length;
+document.write(`My Favorite Phone is : ${favouriteMobile} .<br/>Length of string is : ${lengthOfMobilePhone}`)
+// 3    
+let nationality = "Pakistani";
+let word = 'n';
+let indexOfAWord = nationality.indexOf(word)
+document.write(`Sting : ${nationality}<br/> Index of ${word} : ${indexOfAWord}`)
+// 4
+let greetings = "Hello World";
+let findingWord = "l";
+let lastIndexOfGreetings = greetings.lastIndexOf(findingWord)
+document.write(`String : ${greetings} <br/> Last Index of ${findingWord} : ${lastIndexOfGreetings}`)
+// 5
+let findCharAtIndexNo = 7;
+let userWord = prompt(`Enter anything, I will give you what you have write  on ${findCharAtIndexNo}th index`)
+let indexOfUserWord = userWord.charAt(findCharAtIndexNo)
+document.write(`String : ${userWord} <br/>Character at index ${findCharAtIndexNo} : ${indexOfUserWord}`)
+// 6
+
+// 7
+let city = "Hyderabad is a beautiful city";
+console.log(city);
+city = city.replace("Hyder", "Islam");
+console.log(city);
+
+// 8
+let message = "“Ali and Sami are best friends. They play cricket and football together.”";
+message= message.replaceAll("and","&")
+console.log(message)
+*/
+// 9
+
+// 10
+let questionTen = prompt("Enter here anything you want");
+document.write(questionTen.toUpperCase())
